1e200e0940cca8a7766fa5502967e3b66245d949
ayanaishikawa <s1260003@gmail.com>
2019-07-11 17:24:13 +0900

    Initial revision

---
+++ dice.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class dice{
+    public static void main(String[] args){
+	Random r = new Random();
+
+	int die1 = 1 + r.nextInt(6);
+	int die2 = 1 + r.nextInt(6);
+	
+	System.out.println("Die 1: " + die1);
+	System.out.println("Die 2: " + die2);
+	System.out.println("Total value: " + (die1+die2));	
+    }
+}
